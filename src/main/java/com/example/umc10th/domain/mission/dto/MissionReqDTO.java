4feec75b050f6a28bd348437ecbaaf5bf5352240
Yarlang <jmim613@naver.com>
2026-05-05 23:17:46 +0900

    feat: 6주차 Enum Success -> Status 변경

--- src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -1,13 +1,13 @@
 package com.example.umc10th.domain.mission.dto;
 
-import com.example.umc10th.domain.mission.enums.Success;
+import com.example.umc10th.domain.mission.enums.Status;
 
 import java.time.LocalDate;
 
 public class MissionReqDTO {
     // 미션 성공 승인
     public record MissionSuccessConfirm(
-            Success status,
+            Status status,
             LocalDate completedAt
     ){}
 }
