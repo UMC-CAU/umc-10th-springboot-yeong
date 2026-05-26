23dca54c8fa76195d0749ae5fcb18ac180f6e002
Yarlang <jmim613@naver.com>
2026-05-26 22:47:45 +0900

    refactor : 9주차: 3. SecurityConfig 수정

--- src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -1,23 +1,30 @@
     package com.example.umc10th.global.config;
 
-import com.example.umc10th.global.apiPayload.code.GeneralSuccessCode;
-import com.example.umc10th.global.security.exception.CustomAccessDenied;
-import com.example.umc10th.global.security.exception.CustomEntryPoint;
-import com.example.umc10th.global.util.SecurityResponseUtil;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+    import com.example.umc10th.global.security.exception.CustomAccessDenied;
+    import com.example.umc10th.global.security.exception.CustomEntryPoint;
+    import com.example.umc10th.global.security.filter.JwtAuthFilter;
+    import com.example.umc10th.global.security.service.CustomUserDetailsService;
+    import com.example.umc10th.global.security.util.JwtUtil;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
+    @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final String[] allowUris = {
+        private final JwtUtil jwtUtil;
+        private final CustomUserDetailsService customUserDetailsService;
+
+        private final String[] allowUris = {
             // Swagger 허용
             "/swagger-ui/**",
             "/swagger-resources/**",
@@ -32,16 +39,19 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                // URI 허용 여부
                 .authorizeHttpRequests(requests -> requests
+                        // Public API 허용
                         .requestMatchers(allowUris).permitAll()
+                        // 그 이외 APi는 인증 필요
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .successHandler((request, response, authentication) -> {
-                            SecurityResponseUtil.writeSuccess(response, GeneralSuccessCode.LOGIN);
-                        })
-                        .permitAll()
-                )
+                // 폼 로그인
+                .formLogin(AbstractHttpConfigurer::disable)
+                // 세션
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                // JWT 필터
+                .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
@@ -68,5 +78,10 @@ public class SecurityConfig {
     @Bean
     public CustomEntryPoint customEntryPoint() {
         return new CustomEntryPoint();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter(jwtUtil, customUserDetailsService);
     }
 }
